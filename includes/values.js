a98ddd6e92eba136fc6483b7620205d45ab9efa0
Devanshu <devanshulinux@gmail.com>
2019-01-19 16:08:38 +0530

    meh

--- includes/values.js
+++ includes/values.js
@@ -1,7 +1,7 @@
 module.exports = {
     databaseUrl: 'mongodb://127.0.0.1:27017/ludo2019a',
     secret: 'j41s41r4m',
-    colors: ["red", "green", "yellow", "blue"],
+    colors: ["red", "green", "yellow", "blue", "-", "-", "-", "-", "-", "-", "-"],
     rechargeCommision: 10,
     coinRate: 100 // was 1000 before
 };
